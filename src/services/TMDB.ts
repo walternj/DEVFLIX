0efc9950b1e66bab516ef8214ddb45a56a7aa8e9
Walter Nunes Jr <walternj@hotmail.com>
2020-09-09 01:31:46 +0200

    base_url fixed

--- src/services/TMDB.ts
+++ src/services/TMDB.ts
@@ -1,3 +1,5 @@
+
+const APP_API_BASE='https://api.themoviedb.org/3'
 /*
   - Originals netflix
   - treanding
@@ -24,7 +26,7 @@ export interface FilmData {
 }
 
   const basicFetch = async (endpoint: string) => {
-  const res = await fetch(`${process.env.REACT_APP_API_BASE}${endpoint}`)
+  const res = await fetch(`${APP_API_BASE}${endpoint}`)
   const json = res.json()
 
   return json
