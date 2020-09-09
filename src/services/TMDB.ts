880a9686a1a9eb3008f3b5022e41df219430a2c3
Walter Nunes Jr <walternj@hotmail.com>
2020-09-09 14:14:15 +0200

    no-cors fetch

--- src/services/TMDB.ts
+++ src/services/TMDB.ts
@@ -25,7 +25,7 @@ export interface FilmData {
 
   const basicFetch = async (endpoint: string) => {
   const res = await fetch(`https://api.themoviedb.org/3}${endpoint}`, {
-    mode: "cors"
+    mode: "no-cors"
   })
   const json = res.json()
 
